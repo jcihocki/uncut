1a5c7f20e63a07f13a53064d33839363bdb66638
John Cihocki <cihocki@gmail.com>
2013-05-30 17:18:51 -0400

    naming the fancy processed image route

--- lib/startup_giraffe/uncut/helper.rb
+++ lib/startup_giraffe/uncut/helper.rb
@@ -3,7 +3,7 @@ module StartupGiraffe
     module Helper
       def processed_image_uri_for image_uri, style
         parsed_uri = URI.parse( image_uri )
-        cut_image_url( img_protocol: parsed_uri.protocol, img_host: parsed_uri.host, img_path: image_uri.request_uri, style: style )
+        processed_image_url( img_protocol: parsed_uri.protocol, img_host: parsed_uri.host, img_path: image_uri.request_uri, style: style )
       end
     end
   end
